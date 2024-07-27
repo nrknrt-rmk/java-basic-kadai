41491796a11ff6ddf772089dacdc7fff3a5db1e8
nrknrt-rmk <nrkn.55n940n116a@gmail.com>
2024-07-27 09:42:35 +0900

    kadai_021を追加

---
+++ src/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,19 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21  {
+	
+	public static void main(String[] args) {
+	
+	Dictionary_Chapter21 dict = new Dictionary_Chapter21() ;
+	
+	String[] searchWord = new String[4] ;
+	searchWord[0] ="apple" ;
+	searchWord[1] ="banana" ;
+	searchWord[2] ="grape" ;
+	searchWord[3] ="orange" ;
+		
+	dict.dicList(searchWord);
+		
+	}
+}
+
